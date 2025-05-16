f3180d8dac4ba2d232782d79688674d60f7a470d
MehrShahbaz <mehrshahbaz7@gmail.com>
2025-05-16 12:45:47 +0200

    Add database setup and user model/schema
    - Implement database connection using SQLAlchemy with SQLite.
    - Create User model for database with fields for id, email, and hashed_password.
    - Define Pydantic schemas for user creation and output.

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from app.db.database import Base, engine
+from app.models import user
+
 app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
 
 origins = [
     "http://localhost:3000",
